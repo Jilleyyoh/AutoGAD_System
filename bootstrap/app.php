c4b5fb0ad46661eb171f03d74db5b88bf2a4a6c5
Sienne <siennezorrilla@gmail.com>
2026-06-30 11:25:43 +0800

    debug:bootstrap changes try

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,5 +27,13 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->respond(function (\Symfony\Component\HttpFoundation\Response $response, \Throwable $exception, \Illuminate\Http\Request $request) {
+            if ($response->getStatusCode() === 419) {
+                return back()->with([
+                    'message' => 'The page expired, please refresh and try again.',
+                ]);
+            }
+
+            return $response;
+        });
     })->create();
